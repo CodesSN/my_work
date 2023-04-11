8965a4cad3ca2ae29b6db5f43e65edef4eefb5df
Pavel Montes <pavel.montes@pidelectronics.com>
2023-04-11 12:45:34 -0700

    fix config

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
   aws: {
-    "aws_project_region": "",
-    "aws_cognito_region": "",
-    "aws_user_pools_id": "",
-    "aws_user_pools_web_client_id": ""
+    "aws_project_region": "us-east-1",
+    "aws_cognito_region": "us-east-1",
+    "aws_user_pools_id": "us-east-1_CCNVo7yHO",
+    "aws_user_pools_web_client_id": "7tg888bpopk3vhflgh1t85h2ca",
   },
   s3Bucket: '',
   apiUrl: '',
