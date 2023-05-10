d7cc3dc68e7f37e7312308545d3a4896fd473103
Hibran <hibran.acosta@pidelectronics.com>
2023-05-10 09:49:56 -0600

    fix: se arreglo la busqueda del autocomplete

--- src/app/shared/components/edit_assets/edit.component.ts
+++ src/app/shared/components/edit_assets/edit.component.ts
@@ -43,17 +43,15 @@ export class EditComponent implements OnInit {
     this.options = await this.employeeService.getAllEmployeesAuto();
     this.filteredOptions = this.myControl.valueChanges.pipe(
       startWith(''),
-      map((value) => this._filter(value || ''))
+      map(value => this._filter(value || '')),
     );
   }
 
   private _filter(value: string): string[] {
     const filterValue = value.toLowerCase();
-
-    return this.options.filter((option: string) =>
-      option.toLowerCase().includes(filterValue)
-    );
+    return this.options.filter((option:string) => option.toLowerCase().includes(filterValue));
   }
+
   createTrucksForm(): UntypedFormGroup {
     return this.fb.group({
       id: [this.trucks.id],
