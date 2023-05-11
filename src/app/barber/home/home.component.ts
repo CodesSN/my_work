ee102913f8a93fb103e6255da488b41adc4f7ffc
Pavel Montes <pavel.montes@pidelectronics.com>
2023-05-11 11:03:38 -0600

    El layout del barber home terminado

--- src/app/barber/home/home.component.ts
+++ src/app/barber/home/home.component.ts
@@ -1,10 +1,638 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import {
+  CalendarOptions,
+  DateSelectArg,
+  EventClickArg,
+  EventApi,
+} from '@fullcalendar/core';
+import { HttpClient } from '@angular/common/http';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { EventInput } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import { MatDialog } from '@angular/material/dialog';
+import {
+  UntypedFormBuilder,
+  UntypedFormControl,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
+import { Calendar } from '../calendar/calendar.model';
+import { FormDialogComponent } from '../calendar/form-dialog/form-dialog.component';
+import { CalendarService } from '../calendar/calendar.service';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
+import { INITIAL_EVENTS } from '../calendar/events-util';
+import { MatCheckboxChange } from '@angular/material/checkbox';
+import { UnsubscribeOnDestroyAdapter } from 'src/app/shared/UnsubscribeOnDestroyAdapter';
+import { Direction } from '@angular/cdk/bidi';
+import { MatSidenav } from '@angular/material/sidenav';
+import { Task } from 'src/app/task/task.model';
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexDataLabels,
+  ApexTooltip,
+  ApexYAxis,
+  ApexPlotOptions,
+  ApexStroke,
+  ApexLegend,
+  ApexFill,
+  ApexMarkers,
+  ApexGrid,
+  ApexTitleSubtitle,
+  ApexResponsive,
+} from 'ng-apexcharts';
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  yaxis: ApexYAxis;
+  stroke: ApexStroke;
+  tooltip: ApexTooltip;
+  dataLabels: ApexDataLabels;
+  legend: ApexLegend;
+  responsive: ApexResponsive[];
+  plotOptions: ApexPlotOptions;
+  fill: ApexFill;
+  colors: string[];
+  labels: string[];
+  markers: ApexMarkers;
+  grid: ApexGrid;
+  title: ApexTitleSubtitle;
+};
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent
+  extends UnsubscribeOnDestroyAdapter
+  implements OnInit
+{
+  // Calendar
+  @ViewChild('calendar', { static: false })
+  calendar: Calendar | null;
+  public addCusForm: UntypedFormGroup;
+  dialogTitle: string;
+  filterOptions = 'All';
+  calendarData!: Calendar;
+  filterItems: string[] = [
+    'work',
+    'personal',
+    'important',
+    'travel',
+    'friends',
+  ];
+  calendarEvents?: EventInput[];
+  tempEvents?: EventInput[];
 
+
+  public filters = [
+    { name: 'work', value: 'Work', checked: true },
+    { name: 'personal', value: 'Personal', checked: true },
+    { name: 'important', value: 'Important', checked: true },
+    { name: 'travel', value: 'Travel', checked: true },
+    { name: 'friends', value: 'Friends', checked: true },
+  ];
+
+  // task
+  mode = new UntypedFormControl('side');
+  taskForm: UntypedFormGroup;
+  showFiller = false;
+  isNewEvent = false;
+  userImg?: string;
+  tasks: Task[] = [];
+
+  public areaChartOptions!: Partial<ChartOptions>;
+  public barChartOptions!: Partial<ChartOptions>;
+  public earningOptions!: Partial<ChartOptions>;
+  public performanceRateChartOptions!: Partial<ChartOptions>;
+
+
+  constructor(
+    private fb: UntypedFormBuilder,
+    private dialog: MatDialog,
+    public calendarService: CalendarService,
+    private snackBar: MatSnackBar,
+    private http: HttpClient
+  ) {
+    super();
+    this.dialogTitle = 'Add New Event';
+    const blankObject = {} as Calendar;
+    this.calendar = new Calendar(blankObject);
+    this.addCusForm = this.createCalendarForm(this.calendar);
+    const blank = {} as Task;
+    this.taskForm = this.createFormGroup(blank);
+
+    this.fetch((data: Task[]) => {
+      this.tasks = data;
+    });
+  }
+
+  public ngOnInit(): void {
+    this.chart1();
+    // this.chart2();
+    this.chart3();
+    this.calendarEvents = INITIAL_EVENTS;
+    this.tempEvents = this.calendarEvents;
+    this.calendarOptions.initialEvents = this.calendarEvents;
+  }
+
+  calendarOptions: CalendarOptions = {
+    plugins: [dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin],
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek',
+    },
+    initialView: 'dayGridMonth',
+    weekends: true,
+    editable: true,
+    selectable: true,
+    selectMirror: true,
+    dayMaxEvents: true,
+    select: this.handleDateSelect.bind(this),
+    eventClick: this.handleEventClick.bind(this),
+    eventsSet: this.handleEvents.bind(this),
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  handleDateSelect(selectInfo: DateSelectArg) {
+    this.addNewEvent();
+  }
+
+  addNewEvent() {
+    let tempDirection: Direction;
+    if (localStorage.getItem('isRtl') === 'true') {
+      tempDirection = 'rtl';
+    } else {
+      tempDirection = 'ltr';
+    }
+    const dialogRef = this.dialog.open(FormDialogComponent, {
+      data: {
+        calendar: this.calendar,
+        action: 'add',
+      },
+      direction: tempDirection,
+    });
+
+    this.subs.sink = dialogRef.afterClosed().subscribe((result) => {
+      if (result === 'submit') {
+        this.calendarData = this.calendarService.getDialogData();
+        this.calendarEvents = this.calendarEvents?.concat({
+          // add new event data. must create new array
+          id: this.calendarData.id,
+          title: this.calendarData.title,
+          start: this.calendarData.startDate,
+          end: this.calendarData.endDate,
+          className: this.getClassNameValue(this.calendarData.category),
+          groupId: this.calendarData.category,
+          details: this.calendarData.details,
+        });
+        this.calendarOptions.events = this.calendarEvents;
+        this.addCusForm.reset();
+        this.showNotification(
+          'snackbar-success',
+          'Add Record Successfully...!!!',
+          'bottom',
+          'center'
+        );
+      }
+    });
+  }
+
+  changeCategory(event: MatCheckboxChange, filter: { name: string }) {
+    if (event.checked) {
+      this.filterItems.push(filter.name);
+    } else {
+      this.filterItems.splice(this.filterItems.indexOf(filter.name), 1);
+    }
+    this.filterEvent(this.filterItems);
+  }
+
+  filterEvent(element: string[]) {
+    const list = this.calendarEvents?.filter((x) =>
+      element.map((y?: string) => y).includes(x.groupId)
+    );
+
+    this.calendarOptions.events = list;
+  }
+
+  handleEventClick(clickInfo: EventClickArg) {
+    this.eventClick(clickInfo);
+  }
+
+  eventClick(row: EventClickArg) {
+    const calendarData = {
+      id: row.event.id,
+      title: row.event.title,
+      category: row.event.groupId,
+      startDate: row.event.start,
+      endDate: row.event.end,
+      details: row.event.extendedProps['details'],
+    };
+
+    let tempDirection: Direction;
+    if (localStorage.getItem('isRtl') === 'true') {
+      tempDirection = 'rtl';
+    } else {
+      tempDirection = 'ltr';
+    }
+
+    const dialogRef = this.dialog.open(FormDialogComponent, {
+      data: {
+        calendar: calendarData,
+        action: 'edit',
+      },
+      direction: tempDirection,
+    });
+
+    this.subs.sink = dialogRef.afterClosed().subscribe((result) => {
+      if (result === 'submit') {
+        this.calendarData = this.calendarService.getDialogData();
+        this.calendarEvents?.forEach((element, index) => {
+          if (this.calendarData.id === element.id) {
+            this.editEvent(index, this.calendarData);
+          }
+        }, this);
+        this.showNotification(
+          'black',
+          'Edit Record Successfully...!!!',
+          'bottom',
+          'center'
+        );
+        this.addCusForm.reset();
+      } else if (result === 'delete') {
+        this.calendarData = this.calendarService.getDialogData();
+        this.calendarEvents?.forEach((element) => {
+          if (this.calendarData.id === element.id) {
+            row.event.remove();
+          }
+        }, this);
+
+        this.showNotification(
+          'snackbar-danger',
+          'Delete Record Successfully...!!!',
+          'bottom',
+          'center'
+        );
+      }
+    });
+  }
+
+  editEvent(eventIndex: number, calendarData: Calendar) {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const calendarEvents = this.calendarEvents!.slice();
+    const singleEvent = Object.assign({}, calendarEvents[eventIndex]);
+    singleEvent.id = calendarData.id;
+    singleEvent.title = calendarData.title;
+    singleEvent.start = calendarData.startDate;
+    singleEvent.end = calendarData.endDate;
+    singleEvent.className = this.getClassNameValue(calendarData.category);
+    singleEvent.groupId = calendarData.category;
+    singleEvent['details'] = calendarData.details;
+    calendarEvents[eventIndex] = singleEvent;
+    this.calendarEvents = calendarEvents; // reassign the array
+
+    this.calendarOptions.events = calendarEvents;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  handleEvents(events: EventApi[]) {
+    // this.currentEvents = events;
+  }
+
+  createCalendarForm(calendar: Calendar): UntypedFormGroup {
+    return this.fb.group({
+      id: [calendar.id],
+      title: [
+        calendar.title,
+        [Validators.required, Validators.pattern('[a-zA-Z]+([a-zA-Z ]+)*')],
+      ],
+      category: [calendar.category],
+      startDate: [calendar.startDate, [Validators.required]],
+      endDate: [calendar.endDate, [Validators.required]],
+      details: [
+        calendar.details,
+        [Validators.required, Validators.pattern('[a-zA-Z]+([a-zA-Z ]+)*')],
+      ],
+    });
+  }
+
+  showNotification(
+    colorName: string,
+    text: string,
+    placementFrom: MatSnackBarVerticalPosition,
+    placementAlign: MatSnackBarHorizontalPosition
+  ) {
+    this.snackBar.open(text, '', {
+      duration: 2000,
+      verticalPosition: placementFrom,
+      horizontalPosition: placementAlign,
+      panelClass: colorName,
+    });
+  }
+
+  getClassNameValue(category: string) {
+    let className;
+
+    if (category === 'work') className = 'fc-event-success';
+    else if (category === 'personal') className = 'fc-event-warning';
+    else if (category === 'important') className = 'fc-event-primary';
+    else if (category === 'travel') className = 'fc-event-danger';
+    else if (category === 'friends') className = 'fc-event-info';
+
+    return className;
+  }
+
+  fetch(cb: (i: Task[]) => void) {
+    const req = new XMLHttpRequest();
+    req.open('GET', 'assets/data/task.json');
+    req.onload = () => {
+      const data = JSON.parse(req.response);
+      cb(data);
+    };
+    req.send();
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.tasks, event.previousIndex, event.currentIndex);
+  }
+  toggle(task: { done: boolean }, nav: MatSidenav) {
+    nav.close();
+    task.done = !task.done;
+  }
+  addNewTask(nav: MatSidenav) {
+    this.resetFormField();
+    this.isNewEvent = true;
+    this.dialogTitle = 'New Task';
+    this.userImg = 'assets/images/user/user1.jpg';
+    nav.open();
+  }
+  taskClick(task: Task, nav: MatSidenav): void {
+    this.isNewEvent = false;
+    this.dialogTitle = 'Edit Task';
+    this.userImg = task.img;
+    nav.open();
+    this.taskForm = this.createFormGroup(task);
+  }
+  closeSlider(nav: MatSidenav) {
+    nav.close();
+  }
+  createFormGroup(data: Task) {
+    return this.fb.group({
+      id: [data ? data.id : this.getRandomID()],
+      img: [data ? data.img : 'assets/images/user/user1.jpg'],
+      name: [data ? data.name : null],
+      title: [data ? data.title : null],
+      done: [data ? data.done : null],
+      priority: [data ? data.priority : null],
+      due_date: [data ? data.due_date : null],
+      note: [data ? data.note : null],
+    });
+  }
+  saveTask() {
+    this.tasks.unshift(this.taskForm.value);
+    this.resetFormField();
+  }
+  editTask() {
+    const targetIdx = this.tasks
+      .map((item) => item.id)
+      .indexOf(this.taskForm.value.id);
+    this.tasks[targetIdx] = this.taskForm.value;
+  }
+  deleteTask(nav: MatSidenav) {
+    const targetIdx = this.tasks
+      .map((item) => item.id)
+      .indexOf(this.taskForm.value.id);
+    this.tasks.splice(targetIdx, 1);
+    nav.close();
+  }
+  resetFormField() {
+    this.taskForm.controls['name'].reset();
+    this.taskForm.controls['title'].reset();
+    this.taskForm.controls['done'].reset();
+    this.taskForm.controls['priority'].reset();
+    this.taskForm.controls['due_date'].reset();
+    this.taskForm.controls['note'].reset();
+  }
+  public getRandomID(): number {
+    const S4 = () => {
+      return ((1 + Math.random()) * 0x10000) | 0;
+    };
+    return S4() + S4();
+  }
+
+  private chart1() {
+    this.earningOptions = {
+      series: [
+        {
+          name: '2019',
+          data: [15, 48, 36, 20, 40, 60, 35, 20, 16, 31, 22, 11],
+        },
+        {
+          name: '2018',
+          data: [8, 22, 60, 35, 17, 24, 48, 37, 56, 22, 32, 38],
+        },
+      ],
+      chart: {
+        height: 240,
+        type: 'line',
+        zoom: {
+          enabled: false,
+        },
+        toolbar: {
+          show: false,
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        width: 3,
+        curve: 'smooth',
+        dashArray: [0, 8],
+      },
+      colors: ['#8793ea', '#4caf50'],
+      fill: {
+        opacity: [1, 0.5],
+      },
+      markers: {
+        size: 0,
+        hover: {
+          sizeOffset: 6,
+        },
+      },
+      xaxis: {
+        categories: [
+          'Jan',
+          'Feb',
+          'Mar',
+          'Apr',
+          'May',
+          'Jun',
+          'Jul',
+          'Aug',
+          'Sep',
+          'Oct',
+          'Nov',
+          'Dec',
+        ],
+        labels: {
+          style: {
+            colors: '#8e8da4',
+          },
+        },
+      },
+      yaxis: {
+        labels: {
+          style: {
+            colors: '#8e8da4',
+          },
+        },
+      },
+      grid: {
+        show: true,
+        borderColor: '#9aa0ac',
+        strokeDashArray: 1,
+      },
+      tooltip: {
+        theme: 'dark',
+      },
+    };
+  }
+  // private chart2() {
+  //   this.performanceRateChartOptions = {
+  //     series: [
+  //       {
+  //         name: 'Bill Amount',
+  //         data: [113, 120, 130, 120, 125, 119, 126],
+  //       },
+  //     ],
+  //     chart: {
+  //       height: 380,
+  //       type: 'line',
+  //       dropShadow: {
+  //         enabled: true,
+  //         color: '#000',
+  //         top: 18,
+  //         left: 7,
+  //         blur: 10,
+  //         opacity: 0.2,
+  //       },
+  //       foreColor: '#9aa0ac',
+  //       toolbar: {
+  //         show: false,
+  //       },
+  //     },
+  //     colors: ['#6777EF'],
+  //     dataLabels: {
+  //       enabled: true,
+  //     },
+  //     stroke: {
+  //       curve: 'smooth',
+  //     },
+  //     markers: {
+  //       size: 1,
+  //     },
+  //     xaxis: {
+  //       categories: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+  //       title: {
+  //         text: 'Weekday',
+  //       },
+  //     },
+  //     yaxis: {
+  //       title: {
+  //         text: 'Bill Amount($)',
+  //       },
+  //     },
+  //     grid: {
+  //       show: true,
+  //       borderColor: '#9aa0ac',
+  //       strokeDashArray: 1,
+  //     },
+  //     tooltip: {
+  //       theme: 'dark',
+  //       marker: {
+  //         show: true,
+  //       },
+  //       x: {
+  //         show: true,
+  //       },
+  //     },
+  //   };
+  // }
+
+  private chart3() {
+    this.areaChartOptions = {
+      series: [
+        {
+          name: 'New Clients',
+          data: [31, 40, 28, 51, 42, 85, 77],
+        },
+        {
+          name: 'Old Clients',
+          data: [11, 32, 45, 32, 34, 52, 41],
+        },
+      ],
+      chart: {
+        height: 350,
+        type: 'area',
+        toolbar: {
+          show: false,
+        },
+        foreColor: '#9aa0ac',
+      },
+      colors: ['#4FC3F7', '#7460EE'],
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        curve: 'smooth',
+      },
+      grid: {
+        show: true,
+        borderColor: '#9aa0ac',
+        strokeDashArray: 1,
+      },
+      xaxis: {
+        type: 'datetime',
+        categories: [
+          '2018-09-19',
+          '2018-09-20',
+          '2018-09-21',
+          '2018-09-22',
+          '2018-09-23',
+          '2018-09-24',
+          '2018-09-25',
+        ],
+      },
+      legend: {
+        show: true,
+        position: 'top',
+        horizontalAlign: 'center',
+        offsetX: 0,
+        offsetY: 0,
+      },
+
+      tooltip: {
+        theme: 'dark',
+        marker: {
+          show: true,
+        },
+        x: {
+          show: true,
+        },
+      },
+    };
+  }
 }
