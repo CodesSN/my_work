66b939e7d1c511c76ea3aadb03b8db1e74a9140f
Pavel Montes <pavel.montes@pidelectronics.com>
2023-05-30 17:41:32 -0600

    feat: La carga de los archivos se limito a 5MB

--- src/app/shared/components/file-upload/file-upload.component.ts
+++ src/app/shared/components/file-upload/file-upload.component.ts
@@ -11,13 +11,19 @@ export class FileUploadComponent {
   public fileUrl:any = null;
   public selectedFile: File | null = null;
 
+
   handleFileInput(event: any) {
     this.selectedFile = event.target.files[0];
-    if(this.selectedFile){
+    const MAX_SIZE =  5 * 1024 * 1024; // Limite de 5 MB
+
+    if(this.selectedFile && this.selectedFile.size < MAX_SIZE){
       this.sendFile.emit(this.selectedFile);
       this.fileUrl = URL.createObjectURL(this.selectedFile);
     } else {
+      this.fileUrl = null;
+      this.selectedFile = null;
       this.sendFile.emit(null);
+
     }
   }
 }
